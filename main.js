c397525c8e1b4803bfbda1c200b078d3e0594833
IyadAlanssary <77411974+IyadAlanssary@users.noreply.github.com>
2022-06-06 15:29:53 +0300

    Add enslaved gravity

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ gltfLoader.load("./assets/satellite.gltf",
 (gltf) => {
   satellite = gltf.scene
   satellite.scale.set(0.009, 0.009, 0.009);
-  satellite.position.set(5,0,5)
+  satellite.position.set(0, 6, 0);
   scene.add(satellite);
 },);
 
@@ -106,6 +106,42 @@ let elapsedTime = 1;
 
 function Vortex(satellite) {}
 
+var G = 6.6743e-11;
+var MEarth = 5.972e24;
+var g;
+// function Gravity() {
+//   //g = G * m(earth) / ((((((r^2))))))//////////////////////////////
+//   g =
+//     (G * MEarth) /
+//     Math.sqrt(
+//       Math.pow(satellite.position.x, 2) +
+//         Math.pow(satellite.position.y, 2) +
+//         Math.pow(satellite.position.z, 2)
+//     );
+//     console.log('test')
+
+//   //v = sqrt(G*m(erath)/r)
+//   v = sqrt(
+//     (G * MEarth) / Math.pow(satellite.position.x, 2) +
+//       Math.pow(satellite.position.y, 2) +
+//       Math.pow(satellite.position.z, 2)
+//   );
+//   newY = satellite.position.y - g;
+//   satellite.position.set(new Vector3(satellite.position.x, newY, satellite.position.z))
+//   console.log('test2')
+// }
+
+
+// function distance(obj1, obj2) {
+//   const xDist = obj2.x - obj1.x;
+//   const yDist = obj2.y - obj1.y;
+//   const zDist = obj2.z - obj1.z;
+
+//   return Math.sqrt(
+//     Math.pow(xDist, 2) + Math.pow(yDist, 2) + Math.pow(zDist, 2)
+//   );
+// }
+
 function animate() {
   elapsedTime = clock.getElapsedTime() + 1;
   var delta = clock.getDelta();
@@ -113,6 +149,9 @@ function animate() {
   satellite.position.set(Math.sin(elapsedTime/2) * 3, 5, Math.cos(elapsedTime/2) * 3)
   // satellite.rotation.x += 0.4 * delta;
   // satellite.rotation.y += 0.2 * delta;
+  //Gravity();
+  deltaTime = clock.getElapsedTime() + 1;
+  console.log(deltaTime);
   earth.rotateY(0.004);
   requestAnimationFrame(animate);
   controls.update();
